3c73ba665650a943958e0c1b5fabb5e3f65bd08b
John <john@conducthq.com>
2016-08-09 20:40:46 +1000

    Add missing bourbon and neat. Remove missing component

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
-import StarRating from './starRating/starRating';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
-  StarRating
+  About
 ])
 
 .name;
